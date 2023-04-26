eaf491015de4b9eb46c98d8c5f9e0279f3350375
r00t_k1n9 <46450756+soryecker@users.noreply.github.com>
2023-04-27 00:27:11 +0800

    优化js爬虫

--- JsScan.py
+++ JsScan.py
@@ -1,268 +1,94 @@
-#!/usr/bin/env python"
-# coding: utf-8
-# By Threezh1
-# https://threezh1.github.io/
+import re
+from selenium import webdriver
 
-import requests, argparse, sys, re
-from requests.packages import urllib3
-from urllib.parse import urlparse
-from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
 
-import main
+from main import print_red, print_green
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(epilog='\tExample: \r\npython ' + sys.argv[0] + " -u http://www.baidu.com")
-    parser.add_argument("-u", "--url", help="The website")
-    parser.add_argument("-c", "--cookie", help="The website cookie")
-    parser.add_argument("-f", "--file", help="The file contains url or js")
-    parser.add_argument("-ou", "--outputurl", help="Output file name. ")
-    parser.add_argument("-os", "--outputsubdomain", help="Output file name. ")
-    parser.add_argument("-j", "--js", help="Find in js file", action="store_true")
-    parser.add_argument("-d", "--deep", help="Deep find", action="store_true")
-    return parser.parse_args()
+# def get_js_api(js_code):
+#     # 创建PyExecJS上下文
+#     ctxt = PyExecJS.compile(js_code)
+#     # 获取全局对象
+#     global_obj = ctxt.eval('this')
+#     # 查找所有函数名和变量名
+#     pattern = r'[a-zA-Z]+\w*(?=\()|[a-zA-Z]+\w*(?!=\()'
+#     matches = re.findall(pattern, js_code)
+#     # 过滤出可能为API的名称
+#     apis = []
+#     for match in matches:
+#         if match in global_obj and callable(global_obj[match]):
+#             apis.append(match)
+#     return list(set(apis))
+
+def browse_webpage(url):
+    # 使用Chrome浏览器引擎打开网页
+    options = webdriver.ChromeOptions()  # 创建一个配置对象
+    options.add_argument("--headless")  # 开启无界面模式
+    options.add_argument("--disable-gpu")  # 禁用gpu
+    driver = webdriver.Chrome(chrome_options=options)
+
+    driver.get(url)
+    # 获取网页源代码
+    html = driver.page_source
+    # 关闭浏览器并杀掉Chrome进程
+    driver.quit()
+    webdriver.Chrome(service_log_path=None).service.stop()
+    # 返回网页内容
+    return html
 
 
-# Regular expression comes from https://github.com/GerbenJavado/LinkFinder
-def extract_URL(JS):
-    pattern_raw = r"""
-	  (?:"|')                               # Start newline delimiter
-	  (
-	    ((?:[a-zA-Z]{1,10}://|//)           # Match a scheme [a-Z]*1-10 or //
-	    [^"'/]{1,}\.                        # Match a domainname (any character + dot)
-	    [a-zA-Z]{2,}[^"']{0,})              # The domainextension and/or path
-	    |
-	    ((?:/|\.\./|\./)                    # Start with /,../,./
-	    [^"'><,;| *()(%%$^/\\\[\]]          # Next character can't be...
-	    [^"'><,;|()]{1,})                   # Rest of the characters can't be
-	    |
-	    ([a-zA-Z0-9_\-/]{1,}/               # Relative endpoint with /
-	    [a-zA-Z0-9_\-/]{1,}                 # Resource name
-	    \.(?:[a-zA-Z]{1,4}|action)          # Rest + extension (length 1-4 or action)
-	    (?:[\?|/][^"|']{0,}|))              # ? mark with parameters
-	    |
-	    ([a-zA-Z0-9_\-]{1,}                 # filename
-	    \.(?:php|asp|aspx|jsp|json|
-	         action|html|js|txt|xml)             # . + extension
-	    (?:\?[^"|']{0,}|))                  # ? mark with parameters
-	  )
-	  (?:"|')                               # End newline delimiter
-	"""
-    pattern = re.compile(pattern_raw, re.VERBOSE)
-    result = re.finditer(pattern, str(JS))
-    if result == None:
-        return None
-    js_url = []
-    return [match.group().strip('"').strip("'") for match in result
-            if match.group() not in js_url]
+def parse_js_links(html):
+    # 匹配script标签中src属性的正则表达式
+    pattern = re.compile(r'<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>', re.IGNORECASE)
+    # 从网页源码中查找所有匹配的JS链接
+    js_links = []
+
+    for script in pattern.findall(html):
+        # 匹配script标签中的src属性值的正则表达式
+        src_pattern = re.compile(r'src=["\'](.*?)["\']', re.IGNORECASE)
+        src_match = src_pattern.search(script)
+        if src_match:
+            link = src_match.group(1)
+            if link.startswith("//"):
+                # 去除前两个斜杠
+                link = link[2:]
+            js_links.append(link)
+
+    return js_links
 
 
-# Get the page source
-def Extract_html(URL):
-    header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36",
-        "Cookie": args.cookie}
-    try:
-        raw = requests.get(URL, headers=header, timeout=3, verify=False)
-        raw = raw.content.decode("utf-8", "ignore")
-        return raw
-    except:
-        return None
-
-
-# Handling relative URLs
-def process_url(URL, re_URL):
-    black_url = ["javascript:"]  # Add some keyword for filter url.
-    URL_raw = urlparse(URL)
-    ab_URL = URL_raw.netloc
-    host_URL = URL_raw.scheme
-    if re_URL[0:2] == "//":
-        result = host_URL + ":" + re_URL
-    elif re_URL[0:4] == "http":
-        result = re_URL
-    elif re_URL[0:2] != "//" and re_URL not in black_url:
-        if re_URL[0:1] == "/":
-            result = host_URL + "://" + ab_URL + re_URL
-        else:
-            if re_URL[0:1] == ".":
-                if re_URL[0:2] == "..":
-                    result = host_URL + "://" + ab_URL + re_URL[2:]
-                else:
-                    result = host_URL + "://" + ab_URL + re_URL[1:]
-            else:
-                result = host_URL + "://" + ab_URL + "/" + re_URL
+def extract_domain(url):
+    pattern = r'^http[s]?:\/\/([\w.]+)\/'
+    match = re.match(pattern, url)
+    if match:
+        return match.group(1)
     else:
-        result = URL
-    return result
+        return None
 
 
-def find_last(string, str):
-    positions = []
-    last_position = -1
+def run(url):
+    req = browse_webpage(url)
+    for i in parse_js_links(req, ):
+        if i.startswith("/") and not i.startswith("//"):
+            domain = extract_domain(url)
+            js_url = domain + i
+            print(js_url)
+        else:
+            print(i)
+
+
+def main():
+    print_green('[+]当前模式为js爬虫')
+    print_red('退出请输入"退出/exit"')
     while True:
-        position = string.find(str, last_position + 1)
-        if position == -1: break
-        last_position = position
-        positions.append(position)
-    return positions
-
-
-def find_by_url(url, js=False):
-    if js == False:
-        try:
-            main.print_green("url:" + url)
-        except:
-            main.print_red("操你妈输入这样格式的url：https://www.baidu.com")
-        html_raw = Extract_html(url)
-        if html_raw == None:
-            main.print_red("连接失败" + url)
-            return None
-        # print(html_raw)
-        html = BeautifulSoup(html_raw, "html.parser")
-        html_scripts = html.findAll("script")
-        script_array = {}
-        script_temp = ""
-        for html_script in html_scripts:
-            script_src = html_script.get("src")
-            if script_src == None:
-                script_temp += html_script.get_text() + "\n"
-            else:
-                purl = process_url(url, script_src)
-                script_array[purl] = Extract_html(purl)
-        script_array[url] = script_temp
-        allurls = []
-        for script in script_array:
-            # print(script)
-            temp_urls = extract_URL(script_array[script])
-            if len(temp_urls) == 0: continue
-            for temp_url in temp_urls:
-                allurls.append(process_url(script, temp_url))
-        result = []
-        for singerurl in allurls:
-            url_raw = urlparse(url)
-            domain = url_raw.netloc
-            positions = find_last(domain, ".")
-            miandomain = domain
-            if len(positions) > 1: miandomain = domain[positions[-2] + 1:]
-            # print(miandomain)
-            suburl = urlparse(singerurl)
-            subdomain = suburl.netloc
-            # print(singerurl)
-            if miandomain in subdomain or subdomain.strip() == "":
-                if singerurl.strip() not in result:
-                    result.append(singerurl)
-        return result
-    return sorted(set(extract_URL(Extract_html(url)))) or None
-
-
-def find_subdomain(urls, mainurl):
-    url_raw = urlparse(mainurl)
-    domain = url_raw.netloc
-    miandomain = domain
-    positions = find_last(domain, ".")
-    if len(positions) > 1: miandomain = domain[positions[-2] + 1:]
-    subdomains = []
-    for url in urls:
-        suburl = urlparse(url)
-        subdomain = suburl.netloc
-        # print(subdomain)
-        if subdomain.strip() == "": continue
-        if miandomain in subdomain:
-            if subdomain not in subdomains:
-                subdomains.append(subdomain)
-    return subdomains
-
-
-def find_by_url_deep(url):
-    html_raw = Extract_html(url)
-    if html_raw == None:
-        main.print_red("连接失败：" + url)
-        return None
-    html = BeautifulSoup(html_raw, "html.parser")
-    html_as = html.findAll("a")
-    links = []
-    for html_a in html_as:
-        src = html_a.get("href")
-        if src == "" or src == None: continue
-        link = process_url(url, src)
-        if link not in links:
-            links.append(link)
-    if links == []: return None
-    main.print_red("找到" + str(len(links)) + "条超链接")
-    urls = []
-    i = len(links)
-    for link in links:
-        temp_urls = find_by_url(link)
-        if temp_urls == None: continue
-        main.print_red("Remaining " + str(i) + " | Find " + str(len(temp_urls)) + " URL in " + link)
-        for temp_url in temp_urls:
-            if temp_url not in urls:
-                urls.append(temp_url)
-        i -= 1
-    return urls
-
-
-def find_by_file(file_path, js=False):
-    with open(file_path, "r") as fobject:
-        links = fobject.read().split("\n")
-    if links == []: return None
-    print("ALL Find " + str(len(links)) + " links")
-    urls = []
-    i = len(links)
-    for link in links:
-        if js == False:
-            temp_urls = find_by_url(link)
+        url = input('请输入URL: ')
+        if '退出' == url or 'exit' == url:
+            break
         else:
-            temp_urls = find_by_url(link, js=True)
-        if temp_urls == None: continue
-        main.print_red(str(i) + " Find " + str(len(temp_urls)) + " URL in " + link)
-        for temp_url in temp_urls:
-            if temp_url not in urls:
-                urls.append(temp_url)
-        i -= 1
-    return urls
+            run(url)
+            print('=' * 10)
 
 
-def giveresult(urls, domian):
-    if urls == None:
-        return None
-    print("Find " + str(len(urls)) + " URL:")
-    content_url = ""
-    content_subdomain = ""
-    for url in urls:
-        content_url += url + "\n"
-        print(url)
-    subdomains = find_subdomain(urls, domian)
-    main.print_red("\n找到 " + str(len(subdomains)) + "个子域名:")
-    for subdomain in subdomains:
-        content_subdomain += subdomain + "\n"
-        print(subdomain)
-    if args.outputurl != None:
-        with open(args.outputurl, "a", encoding='utf-8') as fobject:
-            fobject.write(content_url)
-        print("\nOutput " + str(len(urls)) + " urls")
-        print("Path:" + args.outputurl)
-    if args.outputsubdomain != None:
-        with open(args.outputsubdomain, "a", encoding='utf-8') as fobject:
-            fobject.write(content_subdomain)
-        print("\nOutput " + str(len(subdomains)) + " subdomains")
-        print("Path:" + args.outputsubdomain)
-
-
-if __name__ == "__main__":
-    urllib3.disable_warnings()
-    args = parse_args()
-    if args.file == None:
-        if args.deep is not True:
-            urls = find_by_url(args.url)
-            giveresult(urls, args.url)
-        else:
-            urls = find_by_url_deep(args.url)
-            giveresult(urls, args.url)
-    else:
-        if args.js is not True:
-            urls = find_by_file(args.file)
-            giveresult(urls, urls[0])
-        else:
-            urls = find_by_file(args.file, js=True)
-            giveresult(urls, urls[0])
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -18,7 +18,6 @@ logo_pic_t = '''
 
 
 def hprint(context='',  # 内容。
-           tf=True,  # 是否在内容前面带"[*]"
            enter=True,  # 是否在内容之后回车
            logo=False
            ):
@@ -26,37 +25,31 @@ def hprint(context='',  # 内容。
         print_red(logo_pic_h, tf=False)
         print_green(logo_pic_t, tf=False)
     else:
-        if tf:
-            if enter:
-                print(f'[*]{context}')
-            else:
-                print(f'[*]{context}', end='')
+        if enter:
+            print(f'{context}')
         else:
-            if enter:
-                print(f'{context}')
-            else:
-                print(f'{context}', end='')
+            print(f'{context}', end='')
 
 
-def print_red(context='====检测到漏洞====', tf=True):
-    hprint(Fore.RED + context + Fore.RESET, tf=tf)
+def print_red(context='====检测到漏洞===='):
+    hprint(Fore.RED + context + Fore.RESET)
 
 
-def print_green(context='====检测到漏洞====', tf=True):
-    hprint(Fore.GREEN + context + Fore.RESET, tf=tf)
+def print_green(context='====检测到漏洞===='):
+    hprint(Fore.GREEN + context + Fore.RESET)
 
 
-def print_white(context='====检测到漏洞====', tf=True):
-    hprint(Fore.WHITE + context + Fore.RESET, tf=tf)
+def print_white(context='====检测到漏洞===='):
+    hprint(Fore.WHITE + context + Fore.RESET)
 
 
-def print_black(context='====检测到漏洞====', tf=True):
-    hprint(Fore.BLACK + context + Fore.RESET, tf=tf)
+def print_black(context='====检测到漏洞===='):
+    hprint(Fore.BLACK + context + Fore.RESET)
 
 
-def print_yellow(context='====检测到漏洞====', tf=True):
-    hprint(Fore.YELLOW + context + Fore.RESET, tf=tf)
+def print_yellow(context='====检测到漏洞===='):
+    hprint(Fore.YELLOW + context + Fore.RESET)
 
 
-def print_blue(context='====检测到漏洞====', tf=True):
-    hprint(Fore.BLUE + context + Fore.RESET, tf=tf)
+def print_blue(context='====检测到漏洞===='):
+    hprint(Fore.BLUE + context + Fore.RESET)
